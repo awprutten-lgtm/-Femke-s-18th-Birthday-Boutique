f717f3a63b5c55cdd44fb62d297a9e044a5d74f2
awprutten-lgtm <awprutten@gmail.com>
2026-08-15 16:29:48 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // Plak hier alleen jouw Firebase Realtime Database URL.
 // Voorbeeld: https://jouw-project-default-rtdb.europe-west1.firebasedatabase.app
 window.BOUTIQUE_FIREBASE = {
-  databaseURL: ""
+  databaseURL: "https://femke-s-birthday-boutique-default-rtdb.firebaseio.com/"
 };
